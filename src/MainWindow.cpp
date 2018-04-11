9b1e2ad601595a06c2fb762403a83c58cc48b603
David Gal <norulz.gal@gmail.com>
2018-04-11 16:37:38 +0300

    Removed curve drawer files - currently not used

--- src/MainWindow.cpp
+++ src/MainWindow.cpp
@@ -40,7 +40,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include "Version.h"
 #include "AngleConverterDialog.h"
 #include "DataQString.h"
-#include "CurveDrawer.h"
+//#include "CurveDrawer.h"
 
 
 //-----------------------------------------------------------
