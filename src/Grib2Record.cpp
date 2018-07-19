9feb53573b10e9e5650020f3ce9fbc0a83c73e10
didier <didier@sf.net>
2018-07-19 17:10:00 +0200

    uninitialised values (valgrind)

--- src/Grib2Record.cpp
+++ src/Grib2Record.cpp
@@ -21,9 +21,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //----------------------------------------
 Grib2Record::Grib2Record () : GribRecord ()
 {	
-	ok = false;
-	verticalOrientationIsAmbiguous = false;
 }
+
 //----------------------------------------
 Grib2Record::~Grib2Record ()
 {	
