0f6723bf964b80b4bef175641d8fd6ef06e4a241
didier <didier@users.sourceforge.net>
2021-01-01 21:26:33 +0100

    Primary wave arrows, copy and paste typo

--- src/MapDrawer.cpp
+++ src/MapDrawer.cpp
@@ -389,7 +389,7 @@ void MapDrawer::draw_MeteoData_Gridded
 			hasWaveForArrows = plotter->hasData(DataCode(GRB_WAV_DIR,LV_GND_SURF,0));
 			break;
 		case GRB_PRV_WAV_PRIM:
-			hasWaveForArrows = plotter->hasData(DataCode(GRB_WAV_DIR,LV_GND_SURF,0));
+			hasWaveForArrows = plotter->hasData(DataCode(GRB_WAV_PRIM_DIR,LV_GND_SURF,0));
 			break;
 		case GRB_PRV_WAV_SCDY:
 			hasWaveForArrows = plotter->hasData(DataCode(GRB_WAV_SCDY_DIR,LV_GND_SURF,0));
